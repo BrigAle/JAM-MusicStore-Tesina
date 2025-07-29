478f73517c181ae3817b34c7cf9481bca06e9e92
BrigAle <brighentialessandro96@gmail.com>
2025-07-29 17:58:53 +0200

    index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>JAM Music Store</title>
+  <link rel="stylesheet" type="text/css" href="risorse/CSS/style.css" />
+</head>
+
+<body>
+
+  <div id="header">
+    <h1>🎵 JAM Music Store</h1>
+    <ul id="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Catalogo</a></li>
+      <li><a href="#">Offerte</a></li>
+      <li><a href="#">Contatti</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </div>
+
+  <div id="content">
+    <h2>Benvenuto!</h2>
+    <p>Scopri migliaia di brani, album, vinili e accessori per ogni genere musicale.</p>
+  </div>
+
+  <div id="footer">
+    <p>&copy; 2025 JAM Music Store - Tutti i diritti riservati</p>
+  </div>
+
+</body>
+</html>
