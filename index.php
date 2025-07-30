97fe1b93059c215798412b0842af9ad655eca270
BrigAle <brighentialessandro96@gmail.com>
2025-07-30 12:54:41 +0200

    nav menu styling

--- index.php
+++ index.php
@@ -1,36 +1,43 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>JAM Music Store</title>
-  <meta name ="author" content="Brighenti"/>
-  <link rel="stylesheet" type="text/css" href="risorse/CSS/style.css" />
-  <link rel="shortcut icon" href="risorse/IMG/JAM.ico" type="image/x-icon"/>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jam Music Store</title>
+  <link rel="stylesheet" href="risorse\CSS\style.css" />
+  <link rel="shortcut icon" href="risorse\IMG\jam.ico" type="image/x-ico" />
 </head>
 
 <body>
+  <div class="header">
+    <div class="logo">
+      <img src="risorse/IMG/JAM_logo.png" alt="JAM Music Store" />
+    </div>
 
-  <div id="header">
-    <h1>🎵 JAM Music Store</h1>
-    <ul id="nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Catalogo</a></li>
-      <li><a href="#">Offerte</a></li>
-      <li><a href="#">Contatti</a></li>
-      <li><a href="#">Login</a></li>
-    </ul>
+    <div class="navSearch">
+      <input type="text" placeholder="Cerca brani, artisti..." />
+      <button type="submit">🔍</button>
+    </div>
+
+    <div class="navLink">
+      <a href="#"><img src="risorse/IMG/home.png" alt="casetta"></a>
+      <a href="#">Barra di ricerca</a>
+      <a href="#">Login</a>
+      <a href="#">Carrello</a>
+      <a href="#">Catalogo</a>
+    </div>
+
   </div>
 
-  <div id="content">
-    <h2>Benvenuto!</h2>
-    <p>Scopri migliaia di brani, album, vinili e accessori per ogni genere musicale.</p>
-  </div>
+  <div class="content">
 
-  <div id="pdp">
+  </div>
+  <div class="pdp">
     <p>&copy; 2025 JAM Music Store</p>
   </div>
 
 
 </body>
+
 </html>
